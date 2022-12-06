15024aa409b61972123c9775710ff8850c4d0ce8
EkaterinaAsanova <u-kat@mail.ru>
2022-12-06 10:59:22 +0500

    Из трехзначного числа показывает 2-ую цифру

---
+++ Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine ("Введите 3-х значное число");
+string stringNumber = Console.ReadLine();
+
+if (stringNumber[0]== '-') 
+    stringNumber=stringNumber.Remove(0, 1);
+Console.WriteLine ("Вторая цифра равна: " + stringNumber[1]);
