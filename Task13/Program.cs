e767d48b990cfed37fe1d4e6d5d3a5b5ef55e3ef
EkaterinaAsanova <u-kat@mail.ru>
2022-12-06 11:26:17 +0500

    Вывод 3-тий цифры из числа или сообщение,что цифры нет

---
+++ Task13/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine ("Введите число");
+string stringNumber = Console.ReadLine();
+
+if (stringNumber[0] == '-') 
+    stringNumber=stringNumber.Remove(0, 1);
+ 
+if (stringNumber.Length > 2)
+    Console.WriteLine ("Третья цифра из равна: " + stringNumber[2]);
+
+else
+    Console.WriteLine ("В числе нет 3 цифры");
